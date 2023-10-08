3ed8c9bb9330af0f3987e978c56bbc864661666c
ManmanZhang <408903228@qq.com>
2023-10-08 22:15:23 +0800

    更新了 泰坦尼克号生存预测.ipynb. 当中的内容 更改了关于手写的模型评价指标函数的内容

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 泰坦尼克号生存预测.ipynb. 当中的内容 加入了自定义函数 Focal Loss 的内容 "
+git commit -m "更新了 泰坦尼克号生存预测.ipynb. 当中的内容 更改了关于手写的模型评价指标函数的内容 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
