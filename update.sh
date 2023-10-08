32b803906b806a4c3b197c2b0257897e7145dc8a
ManmanZhang <408903228@qq.com>
2023-10-08 21:26:41 +0800

    更新了 泰坦尼克号生存预测.ipynb. 当中的内容 加入了自定义函数 Focal Loss 的内容

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
-git init
+# git init
 git add .
-git commit -m "更新了 Xgboosting分类问题使用方法.ipynb. 当中的内容 "
-git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
+git commit -m "更新了 泰坦尼克号生存预测.ipynb. 当中的内容 加入了自定义函数 Focal Loss 的内容 "
+# git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
 # git push -f origin master # 强制推送
