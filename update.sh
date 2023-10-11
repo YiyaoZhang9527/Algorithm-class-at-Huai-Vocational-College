fb7bf9a97d98702d3a33a21d6f96dc5b11a7fbe2
ManmanZhang <408903228@qq.com>
2023-10-11 20:12:54 +0800

    更新了 数据预处理，降维算法当中的多个项目的资料内容

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 多目标遗传算法的文件内容"
+git commit -m "更新了 数据预处理，降维算法当中的多个项目的资料内容"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
