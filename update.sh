4ded50f2f5b1768a19ef149135f2edeaced817bf
ManmanZhang <408903228@qq.com>
2023-10-11 20:31:13 +0800

    更新了 Xgboost 如何用sympy模块对损失函数求1阶和2阶导

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 数据预处理，降维算法当中的多个项目的资料内容"
+git commit -m "更新了 Xgboost 如何用sympy模块对损失函数求1阶和2阶导 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
