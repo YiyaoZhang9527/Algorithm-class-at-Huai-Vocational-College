1fe7f4e871200a5373932f20f1bedbb3a76a2c5b
ManmanZhang <408903228@qq.com>
2023-10-12 22:52:55 +0800

    更新了 聚类算法 当中的内容，对原本整个ipynb文件做了拆分，方便后续更新各个算法的调参说明和原理

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 xgboost 分类资料当中的 关于泰勒展开的原理的内容 "
+git commit -m "更新了 聚类算法 当中的内容，对原本整个ipynb文件做了拆分，方便后续更新各个算法的调参说明和原理 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
