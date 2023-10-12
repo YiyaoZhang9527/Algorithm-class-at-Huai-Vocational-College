d443ad1a22c29e7bb7e89705361f6747f648e233
ManmanZhang <408903228@qq.com>
2023-10-12 21:46:42 +0800

    更新了 xgboost 分类资料当中的 关于泰勒展开的原理的内容

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m " 测试 rsync 同步文件过滤.git/文件夹的效果"
+git commit -m "更新了 xgboost 分类资料当中的 关于泰勒展开的原理的内容 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
