cf5428cf2eb4c5a4824fedd94a2fb3c6c1ab6d71
ManmanZhang <408903228@qq.com>
2023-10-12 16:54:25 +0800

    测试 rsync 同步文件过滤.git/文件夹的效果

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m " git 更新修复测试 2 "
+git commit -m " 测试 rsync 同步文件过滤.git/文件夹的效果"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
