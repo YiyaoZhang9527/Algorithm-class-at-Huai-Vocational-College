eee766be0ca5970bfcfa6eb28208bd25fea72ab6
ManmanZhang <408903228@qq.com>
2023-10-13 10:45:02 +0800

    更新了 Xgboosting分类资料.md 加入了 两篇关于不同情况下损失函数选择方法的文章

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 特征选择资料.md 加入了 lasso 回归选择特征的的实现文章 "
+git commit -m "更新了 Xgboosting分类资料.md 加入了 两篇关于不同情况下损失函数选择方法的文章 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
