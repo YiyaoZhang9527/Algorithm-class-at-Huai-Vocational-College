f33eb469c761c9df270cc182f7f3f9331a67c537
ManmanZhang <408903228@qq.com>
2023-10-14 16:13:14 +0800

    更新了 粗糙集基础知识.ipynb 更新了上近似与下近似的计算函数

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 粗糙集基础知识.ipynb 更新了等价属性查询的函数 "
+git commit -m "更新了 粗糙集基础知识.ipynb 更新了上近似与下近似的计算函数 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
