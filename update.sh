58b185c7e337f75ac81cf167f7f4059735ed700a
ManmanZhang <408903228@qq.com>
2023-10-14 19:38:53 +0800

    更新了 README.md 文件，加入了学习机器学习所需要的基础数学概念视频课程

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 粗糙集基础知识.ipynb 更新了上近似与下近似的计算函数 "
+git commit -m "更新了 README.md 文件，加入了学习机器学习所需要的基础数学概念视频课程 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
