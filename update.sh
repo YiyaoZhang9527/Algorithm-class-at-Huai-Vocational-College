5e2fedf634c0126cf71c8566c8b34e894b0e4b67
ManmanZhang <408903228@qq.com>
2023-10-16 15:49:46 +0800

    更新了 README.md

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 关于朴素贝叶斯的原理 的一篇文章"
+git commit -m "更新了 README.md "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
