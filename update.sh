d97dd1304f155b677dc886d200dd967f593d7dfa
ManmanZhang <408903228@qq.com>
2023-10-17 15:35:53 +0800

    更新了 粗糙集的三个函数 正域 负域 和 边界

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 P检验的两种方法 "
+git commit -m "更新了 粗糙集的三个函数 正域 负域 和 边界"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
