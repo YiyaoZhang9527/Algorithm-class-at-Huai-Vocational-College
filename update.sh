1fffbd0a63bdd4eb1fa9796f22a491d82ff4d842
ManmanZhang <408903228@qq.com>
2023-10-18 09:28:42 +0800

    更新了 粗糙集的一个函数 判断是B是否是A的约简

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 粗糙集的一个函数 判断是否是约简集"
+git commit -m "更新了 粗糙集的一个函数 判断是B是否是A的约简"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
