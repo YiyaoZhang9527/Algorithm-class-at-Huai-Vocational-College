05cf664b626605986c458afb51f90e2a0734e1b4
ManmanZhang <408903228@qq.com>
2023-10-18 16:47:19 +0800

    更新了 粗糙集 计算属性集A的核的函数,取消了罗素悖论

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 粗糙集的一个函数 计算属性集A的核 "
+git commit -m "更新了 粗糙集 计算属性集A的核的函数,取消了罗素悖论 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
