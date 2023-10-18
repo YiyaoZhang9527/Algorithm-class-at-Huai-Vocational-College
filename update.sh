59a62e61871fdd69d03a4af3f4da13c04672cebf
ManmanZhang <408903228@qq.com>
2023-10-18 16:35:05 +0800

    更新了 粗糙集的一个函数 计算属性集A的核

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 粗糙集的一个函数 计算粗糙集的精度，粗糙度，和质量"
+git commit -m "更新了 粗糙集的一个函数 计算属性集A的核 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
