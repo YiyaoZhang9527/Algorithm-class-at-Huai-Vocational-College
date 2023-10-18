7de987236e0618c6addc38334bfb0c5009c6c5f3
ManmanZhang <408903228@qq.com>
2023-10-18 09:51:00 +0800

    更新了 粗糙集的一个函数 计算粗糙集的精度，粗糙度，和质量

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 粗糙集的一个函数 判断是B是否是A的约简"
+git commit -m "更新了 粗糙集的一个函数 计算粗糙集的精度，粗糙度，和质量"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
