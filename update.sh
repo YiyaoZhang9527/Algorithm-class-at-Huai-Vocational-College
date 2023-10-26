f4f1956293a47129478c9ea253de5b4c1056d99c
ManmanZhang <408903228@qq.com>
2023-10-26 21:04:00 +0800

    更新了 线性支持向量机.ipynb  sklearn当中的两种实现方法

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 matplotlib中文显示乱码处理.ipynb 在linux域macos系统显示之间切换的问题 "
+git commit -m "更新了 线性支持向量机.ipynb  sklearn当中的两种实现方法 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
