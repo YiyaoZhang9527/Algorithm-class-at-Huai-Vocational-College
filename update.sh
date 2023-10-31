96686ee073a381f660e2971c6d8a62eb37aa4bc0
ManmanZhang <408903228@qq.com>
2023-10-31 16:57:45 +0800

    更新了 卡方检验的基础知识 等待代码实现案例

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 DNN 的代码实现测试 更新了单批次训练的函数"
+git commit -m "更新了 卡方检验的基础知识 等待代码实现案例"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
