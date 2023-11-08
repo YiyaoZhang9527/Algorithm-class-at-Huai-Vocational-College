455ad90b2ca9ea80f84a7c0852413bcfde699544
ManmanZhang <408903228@qq.com>
2023-11-08 12:48:37 +0800

    更新了 回归算法的一级目录 尚未填充

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 数据预处理的分支 将降维算法加入其中"
+git commit -m "更新了 回归算法的一级目录 尚未填充"
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
