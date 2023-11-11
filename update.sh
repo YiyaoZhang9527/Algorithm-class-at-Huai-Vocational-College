867464cc3a986d10985d7e5762a1daf5fe2b2166
ManmanZhang <408903228@qq.com>
2023-11-11 19:46:53 +0800

    更新了 README.md

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 回归算法的一级目录 尚未填充"
+git commit -m "更新了 README.md "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
