eb79d9c9864c68f35d702787368a288dfe1d63ce
ManmanZhang <408903228@qq.com>
2024-05-04 18:12:52 +0800

    更新了 组合数计算，README里更新了一门新的数学课程地址

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 # git init
 git add .
-git commit -m "更新了 更新了遗传算法，模拟退活，贝叶斯估计对参数优化的脚本实例 "
+git commit -m "更新了 组合数计算，README里更新了一门新的数学课程地址 "
 # git remote add origin https://github.com/YiyaoZhang9527/Algorithm-class-at-Huai-Vocational-College.git
 git branch -M master
 git push -u origin master
